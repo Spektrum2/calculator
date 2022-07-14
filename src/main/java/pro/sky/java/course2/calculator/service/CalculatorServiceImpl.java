489f3cf56e18c36ae3c2f694b0fa684ab7df470e
admin <promitay@gmail.com>
2022-07-14 14:08:58 +0300

    Домашняя работа 2.3. Введение в Maven и Spring Boot

--- src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorServiceImpl.java
@@ -30,7 +30,7 @@ public class CalculatorServiceImpl implements CalculatorService {
     @Override
     public String divide(double num1, double num2) {
         if (num1 == 0 || num2 == 0) {
-            return "Ошибка ввода. Значение не должно быть рано нулю!";
+            return "Ошибка ввода. Значение не должно быть равно нулю!";
         }
         double total = num1 / num2;
         return num1 + " / " + num2 + " = " + total;
